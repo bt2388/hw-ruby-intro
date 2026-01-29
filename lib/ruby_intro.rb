3f5aeb4c8228c84557b21d9eebfbe1b09cc999df
benjamintian2005 <benjamin.tian.2005@gmail.com>
2026-01-29 17:28:38 -0500

    part 2 and 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -8,7 +8,7 @@ def sum(arr)
   arr.each do |num|
     total += num
   end
-  total
+  return total
 end
 
 def max_2_sum(arr)
@@ -17,7 +17,7 @@ def max_2_sum(arr)
   return arr[0] if arr.length == 1
 
   sorted_arr = arr.sort
-  sorted_arr[-1] + sorted_arr[-2]
+  return sorted_arr[-1] + sorted_arr[-2]
 end
 
 def sum_to_n?(arr, n)
@@ -28,25 +28,47 @@ def sum_to_n?(arr, n)
     return true if seen[complement]
     seen[num] = true
   end
-  false
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  s= "Hello, #{name}"
+  s
 end
 
 def starts_with_consonant?(s)
   # YOUR CODE HERE
+  return false if s.empty?
+  first_char = s[0].downcase
+  return false unless first_char =~ /[a-z]/
+  return !(first_char =~ /[aeiou]/)
 end
 
 def binary_multiple_of_4?(s)
   # YOUR CODE HERE
+  return false if s.empty?
+  return false unless s =~ /^[01]+$/
+  return s.end_with?('00')
 end
 
 # Part 3
 
 class BookInStock
   # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new("ISBN cannot be empty") if isbn.empty?
+    raise ArgumentError.new("Price must be greater than 0") if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  attr_reader :isbn
+  attr_reader :price
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+  
 end
